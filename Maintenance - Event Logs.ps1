79b1b153bb4e2fe5fd6c74a67b29ba18a5c93c11
Get-LocalUser <121067700+Get-LocalUser@users.noreply.github.com>
2025-08-06 13:11:26 -0700

    Update Maintenance - Event Logs.ps1

--- Maintenance - Event Logs.ps1
+++ Maintenance - Event Logs.ps1
@@ -1,4 +1,3 @@
-# Define log names
 $LogNamesTable = @{
     1 = 'Application'
     2 = 'Security'
@@ -6,17 +5,13 @@ $LogNamesTable = @{
     4 = 'System'
 }
 
-# Write out the log names
 Write-Host "Available log names are:"
 $LogNamesTable.Values | ForEach-Object { Write-Host $_ -ForegroundColor Magenta }
 
-# Get the log you want to check and see if it matches any available logs
 $Question = Read-Host "Which log would you like to check?"
 
 if ($LogNamesTable.Values -contains $Question) {
-    #Write-Host "Checking 'Application' logs.." -ForegroundColor Yellow
 
-    # Define log levels
     $Levels = @{
         1 = 'Critical'
         2 = 'Error'
@@ -26,19 +21,15 @@ if ($LogNamesTable.Values -contains $Question) {
         6 = 'Debug'
     }
 
-    # Write out the log levels
     Write-Host "`nAvailable log levels are:"
     $Levels.Values | ForEach-Object { Write-Host $_ -ForegroundColor Magenta }
 
-    # Get the log level you want to check
     $Level = Read-Host "Which log level would you like to check?"
     [int]$Events = Read-Host "In number format, how many logs would you like displayed?"
 
     if ($Levels.Values -contains $Level) {
-        # Convert level name to number
         $LevelNumber = ($Levels.GetEnumerator() | Where-Object { $_.Value -eq $Level }).Key
 
-        # Get the logs for the specified logname and level
         Get-WinEvent -FilterHashtable @{
             LogName = $Question
             Level = $LevelNumber
